67e8e5dbbaa0c3880b96e8adcd5a3737200f4158
Patrick Kubiak <epicpatka@gmail.com>
2025-10-30 16:10:01 -0400

    Update cacerts script

--- src/client/sdkman/sdk-install-cacerts.sh
+++ src/client/sdkman/sdk-install-cacerts.sh
@@ -2,9 +2,14 @@
 
 ### Auto import Cloudflare Zero Trust certificate after install
 
+# exit immediately on error
+set -e
+
+# load sdkman install script and replace __sdk_install with __sdk_builtin_install
 source <(sed 's/__sdk_install/__sdk_builtin_install/g' "${SDKMAN_DIR}/src/sdkman-install.sh")
 
 function __sdk_install() {
+  # call original sdkman install function
   __sdk_builtin_install "$@"
 
   # only run for java installs
@@ -12,17 +17,14 @@ function __sdk_install() {
     return 1
   fi
 
-  local exit_code=$?
+  # get java home
+  local java_home="$(sdk home "${1}" "${2}")"
 
-  if ((exit_code==0)); then
-    local java_home
-    java_home="$(sdk home "${1}" "${2}")"
+  # format cloudflare certificate
+  openssl x509 -in /usr/local/share/ca-certificates/managed-warp.pem -inform pem -out "${SDKMAN_DIR}/etc/managed-warp.der" -outform der
+  # import cloudflare certificate
+  "${java_home}/bin/keytool" -import -trustcacerts -alias 'Cloudflare Root CA' -file "${SDKMAN_DIR}/etc/managed-warp.der" -cacerts -storepass changeit -noprompt 2>&1
 
-    # format cloudflare certificate
-    openssl x509 -in /usr/local/share/ca-certificates/managed-warp.pem -inform pem -out "${SDKMAN_DIR}/etc/managed-warp.der" -outform der
-    # import cloudflare certificate
-    "${java_home}/bin/keytool" -import -trustcacerts -alias 'Cloudflare Root CA' -file "${SDKMAN_DIR}/etc/managed-warp.der" -cacerts -storepass changeit -noprompt 2>&1
-  fi
-
-  return "${exit_code}"
+  # return status of last command
+  return $?
 }
