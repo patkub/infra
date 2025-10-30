35296dc47406ff6f20548bb96767cc0793a455ba
Patrick Kubiak <epicpatka@gmail.com>
2025-10-30 16:34:06 -0400

    Update return codes for cacerts script

--- src/client/sdkman/sdk-install-cacerts.sh
+++ src/client/sdkman/sdk-install-cacerts.sh
@@ -2,15 +2,19 @@
 
 ### Auto import Cloudflare Zero Trust certificate after install
 
-# exit immediately on error
-set -e
-
 # load sdkman install script and replace __sdk_install with __sdk_builtin_install
 source <(sed 's/__sdk_install/__sdk_builtin_install/g' "${SDKMAN_DIR}/src/sdkman-install.sh")
 
 function __sdk_install() {
+  local exit_code
+
   # call original sdkman install function
   __sdk_builtin_install "$@"
+  # ensure last command succeeded
+  exit_code=$?
+  if [[ $exit_code -ne 0 ]]; then
+    return $exit_code
+  fi
 
   # only run for java installs
   if [ "$1" != "java" ]; then
@@ -19,12 +23,28 @@ function __sdk_install() {
 
   # get java home
   local java_home="$(sdk home "${1}" "${2}")"
+  # ensure last command succeeded
+  exit_code=$?
+  if [[ $exit_code -ne 0 ]]; then
+    return $exit_code
+  fi
 
   # format cloudflare certificate
   openssl x509 -in /usr/local/share/ca-certificates/managed-warp.pem -inform pem -out "${SDKMAN_DIR}/etc/managed-warp.der" -outform der
+  # ensure last command succeeded
+  exit_code=$?
+  if [[ $exit_code -ne 0 ]]; then
+    return $exit_code
+  fi
+
   # import cloudflare certificate
   "${java_home}/bin/keytool" -import -trustcacerts -alias 'Cloudflare Root CA' -file "${SDKMAN_DIR}/etc/managed-warp.der" -cacerts -storepass changeit -noprompt 2>&1
+  # ensure last command succeeded
+  exit_code=$?
+  if [[ $exit_code -ne 0 ]]; then
+    return $exit_code
+  fi
 
   # return status of last command
-  return $?
+  return $exit_code
 }
