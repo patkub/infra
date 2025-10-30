cd3b848c37446716fd106d3a07d74d3ecc24e5ec
Patrick Kubiak <epicpatka@gmail.com>
2025-10-30 13:37:49 -0400

    Update keytool cacerts command

--- src/client/sdkman/sdk-install-cacerts.sh
+++ src/client/sdkman/sdk-install-cacerts.sh
@@ -21,7 +21,7 @@ function __sdk_install() {
     # format cloudflare certificate
     openssl x509 -in /usr/local/share/ca-certificates/managed-warp.pem -inform pem -out "${SDKMAN_DIR}/etc/managed-warp.der" -outform der
     # import cloudflare certificate
-    "${java_home}/bin/keytool" -import -trustcacerts -alias 'Cloudflare Root CA' -file "${SDKMAN_DIR}/etc/managed-warp.der" -keystore "$java_home/lib/security/cacerts" -storepass changeit -noprompt 2>&1
+    "${java_home}/bin/keytool" -import -trustcacerts -alias 'Cloudflare Root CA' -file "${SDKMAN_DIR}/etc/managed-warp.der" -cacerts -storepass changeit -noprompt 2>&1
   fi
 
   return "${exit_code}"
