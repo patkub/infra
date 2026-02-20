1c68a940f57a1ca2d3e2e61da5efe4416c5be497
Patrick Kubiak <epicpatka@gmail.com>
2026-02-19 20:04:37 -0500

    update sdkman patch script

--- scripts/client/sdkman/patch.sh
+++ scripts/client/sdkman/patch.sh
@@ -2,8 +2,11 @@
 
 # Patch SDKMAN! to automatically install Cloudflare Zero Trust certificate when installing a Java JDK.
 
+# Get absolute path to this script
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+
 # copy sdkman cacerts script to sdkman ext directory
-cp "$(dirname "$0")/sdk-install-cacerts.sh" "$HOME/.sdkman/ext/"
+cp "$SCRIPT_DIR/sdk-install-cacerts.sh" "$HOME/.sdkman/ext/"
 
 # append sdkman cacerts script to bashrc
 if ! grep -q -x 'source "$HOME/.sdkman/ext/sdk-install-cacerts.sh"' "$HOME/.bashrc"; then
