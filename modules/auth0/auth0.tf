c9c056dc9410d45437cc0ea62ae1e05d78666d27
Patrick Kubiak <epicpatka@gmail.com>
2026-03-01 17:40:40 -0500

    update cloudflare access auth0 client

--- modules/auth0/auth0.tf
+++ modules/auth0/auth0.tf
@@ -17,34 +17,23 @@ resource "auth0_client" "cloudflare_access" {
   callbacks                                            = ["https://epicpatka.cloudflareaccess.com/cdn-cgi/access/callback"]
   client_aliases                                       = []
   client_metadata                                      = {}
-  compliance_level                                     = null
   cross_origin_auth                                    = false
-  cross_origin_loc                                     = null
-  custom_login_page                                    = null
-  custom_login_page_on                                 = true
-  description                                          = null
-  encryption_key                                       = null
-  form_template                                        = null
   grant_types                                          = ["authorization_code", "refresh_token"]
-  initiate_login_uri                                   = null
+  initiate_login_uri                                   = "https://epicpatka.cloudflareaccess.com/"
   is_first_party                                       = true
   is_token_endpoint_ip_header_trusted                  = false
-  logo_uri                                             = null
   name                                                 = "Cloudflare Access"
   oidc_conformant                                      = true
-  organization_require_behavior                        = null
-  organization_usage                                   = null
+  organization_usage                                   = "deny"
   require_proof_of_possession                          = false
   require_pushed_authorization_requests                = false
-  resource_server_identifier                           = null
   skip_non_verifiable_callback_uri_confirmation_prompt = false
-  sso                                                  = false
+  sso                                                  = true
   sso_disabled                                         = false
   web_origins                                          = []
   default_organization {
     disable         = true
     flows           = []
-    organization_id = null
   }
   jwt_configuration {
     alg                 = "RS256"
@@ -64,12 +53,12 @@ resource "auth0_client" "cloudflare_access" {
     }
   }
   refresh_token {
-    expiration_type              = "non-expiring"
+    expiration_type              = "expiring"
     idle_token_lifetime          = 2592000
-    infinite_idle_token_lifetime = true
-    infinite_token_lifetime      = true
+    infinite_idle_token_lifetime = false
+    infinite_token_lifetime      = false
     leeway                       = 0
-    rotation_type                = "non-rotating"
+    rotation_type                = "rotating"
     token_lifetime               = 31557600
   }
 }
