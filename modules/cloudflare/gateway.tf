7dfba3ebccc96d2d023322ddb5e5fcc068fedb03
Patrick Kubiak <epicpatka@gmail.com>
2026-03-03 19:13:04 -0500

    Order of network policies

--- modules/cloudflare/gateway.tf
+++ modules/cloudflare/gateway.tf
@@ -23,12 +23,24 @@ locals {
   ])...)
 }
 
+# Network Policy to allow Access Infrastructure Target
+resource "cloudflare_zero_trust_gateway_policy" "zero_trust_access_infrastructure_target" {
+  account_id  = var.cf_account_id
+  name        = "Access Infrastructure Target"
+  description = "Access Infrastructure Target"
+  precedence  = 0
+  action      = "allow"
+  enabled     = true
+  traffic     = "access.target"
+  filters     = ["l4"]
+}
+
 # DNS Policy to block ads and security risks
 resource "cloudflare_zero_trust_gateway_policy" "zero_trust_block_categories" {
   account_id  = var.cf_account_id
   name        = "AdBlock"
   description = "Block ads and security risks"
-  precedence  = 0
+  precedence  = 1
   action      = "block"
   enabled     = true
   filters     = ["dns"]
@@ -39,18 +51,6 @@ resource "cloudflare_zero_trust_gateway_policy" "zero_trust_block_categories" {
     local.subcategories_map["Parked & For Sale Domains"]
     # "Security Categories" in "All security risks"
   ])}}) and any(dns.security_category[*] in {${join(" ", values(local.categories_map["Security threats"]))}})"
-}
-
-# Network Policy to allow Access Infrastructure Target
-resource "cloudflare_zero_trust_gateway_policy" "zero_trust_access_infrastructure_target" {
-  account_id  = var.cf_account_id
-  name        = "Access Infrastructure Target"
-  description = "Access Infrastructure Target"
-  precedence  = 1
-  action      = "allow"
-  enabled     = true
-  traffic     = "access.target"
-  filters     = ["l4"]
 }
 
 # Cloudflare Gateway Settings
