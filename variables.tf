7f3400d06dcb2960ca68892b60f628feee194478
Patrick Kubiak <epicpatka@gmail.com>
2026-09-22 09:56:09 -0400

    Terraform unit tests (#49)
    * Terraform unit tests
    
    Ensure MAX_LOGINS_WITHOUT_PASSKEY is valid
    
    * Add test vars for unit tests

--- variables.tf
+++ variables.tf
@@ -54,4 +54,9 @@ variable "MAX_LOGINS_WITHOUT_PASSKEY" {
   description = "Maximum number of logins without a passkey before enforcement"
   type        = string
   sensitive   = true
+
+  validation {
+    condition     = can(regex("^[1-9][0-9]*$", var.MAX_LOGINS_WITHOUT_PASSKEY))
+    error_message = "MAX_LOGINS_WITHOUT_PASSKEY must be a positive whole number (e.g. \"3\")."
+  }
 }
